8e21a8e49880cccf5041eac316b445202a66c854
Brian Neal <brianbneal@gmail.com>
2023-12-12 08:58:40 -0600

    Add project files.

---
+++ BookifyDomain/Apartments/Currency.cs
@@ -0,0 +1,21 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public record Currency
+{
+    internal static Currency None => new(string.Empty);
+    public static Currency Usd => new("USD");
+    public static Currency Eur => new("EUR");
+    private Currency(string code) => Code = code;
+    public string Code { get; init; }
+
+    public static Currency FromCode(string code)
+    {
+        return All.FirstOrDefault(x => x.Code == code) ??
+            throw new ApplicationException("The currency code is invalid");
+    }
+    public static readonly IReadOnlyCollection<Currency> All = new[]
+    {
+        Usd,
+        Eur,
+    };
+}
