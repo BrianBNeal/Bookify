e250ee5e3f50cd9bb1c0944adeefb44a6b9edba9
Brian Neal <brianbneal@gmail.com>
2023-12-18 07:53:09 -0600

    created infrastructure project and first 2 services

---
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Email;
+using Bookify.Infrastructure.Clock;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+namespace Bookify.Infrastructure;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+        services.AddTransient<IEmailService, IEmailService>();
+
+        return services;
+    }
+}
