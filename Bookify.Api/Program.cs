3a6eb56f68a55d911bddd1426a053bc2cd7b5ff5
Brian Neal <brianbneal@gmail.com>
2023-12-19 12:20:48 -0600

    infrastructure and api

--- Bookify.Api/Program.cs
+++ Bookify.Api/Program.cs
@@ -1,9 +1,31 @@
+using Bookify.Api.Abstractions;
+using Bookify.Api.Extensions;
+using Bookify.Application;
+using Bookify.Infrastructure;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(x =>
+        {
+            x.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
+            x.JsonSerializerOptions.Converters.Add(new NullableDateOnlyJsonConverter());
+        });
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.MapType<DateOnly>(() => new OpenApiSchema
+        {
+            Type = "string",
+            Format = "date"
+        });
+    });
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
@@ -11,6 +33,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.ApplyMigrations();
 }
 
 app.UseHttpsRedirection();
